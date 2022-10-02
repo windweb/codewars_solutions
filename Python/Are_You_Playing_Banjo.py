5757f27326b6a7f533671dda8188cdd7eb06caa2
windweb <github@aidarov.ru>
2022-10-02 20:14:39 +0300

    init "Are You Playing Banjo?"

---
+++ Python/Are_You_Playing_Banjo.py
@@ -0,0 +1,11 @@
+def are_you_playing_banjo(name):
+    if name[0] == 'R' or name[0] == 'r':
+        print(f'{name} plays banjo')
+    else:
+        print(f'{name} does not play banjo')
+
+
+are_you_playing_banjo("martin")
+are_you_playing_banjo("Rikke")
+are_you_playing_banjo("bravo")
+are_you_playing_banjo("rolf")
